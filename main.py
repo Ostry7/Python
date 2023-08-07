d7f0d731af283f3b7a445e9a82c4dba3510e5ec9
Marek Ostrowski <ostrowskimarek7@outlook.com>
2023-08-07 22:55:43 +0200

    2023-08-07

--- main.py
+++ main.py
@@ -16,47 +16,50 @@
 
 #1.
 
-#import math
-#
-#print ("Select A: ")
-#a = int(input())
-#print ("Select B: ")
-#b = int(input())
-#print ("Select ANGLE: ")
-#angle = int(input())
-##wynik = triangle_area(a,b,angle)
-#angle_rad = math.radians(angle)
-#triangleArea = (a * b * 0.5 * math.sin(angle_rad))
-#
-#print(f"Pole trojkata wynosi: {triangleArea}")
+import math
+
+print ("Select A: ")
+a = int(input())
+print ("Select B: ")
+b = int(input())
+print ("Select ANGLE: ")
+angle = int(input())
+#wynik = triangle_area(a,b,angle)
+angle_rad = math.radians(angle)
+triangleArea = (a * b * 0.5 * math.sin(angle_rad))
+
+print(f"Pole trojkata wynosi: {triangleArea}")
 
 
 #2
 
-#def Fahr_calc():
-#    print(f"Select temperature in Celsious: ")
-#    Celc = float(input())
-#    calculate = (Celc * (9/5) + 32)
-#    print(f"{Celc} Celsious = {calculate} Fahrenheita")
-#Fahr_calc()
+def Fahr_calc():
+    print(f"Select temperature in Celsious: ")
+    Celc = float(input())
+    calculate = (Celc * (9/5) + 32)
+    print(f"{Celc} Celsious = {calculate} Fahrenheita")
+Fahr_calc()
 
 #3
-#
-#def input_calc():
-#    with open(r"test.txt", 'r') as file:
-#        count = 0
-#        for line in file:
-#            if line != "\n":
-#                count += 1
-#    num_lines = count
-#    file = open("test.txt", "r")
-#    data = file.read()
-#    num_of_characters = len(data)
-#    num_of_words = data.split()
-#    print(f"Number of characters: {num_of_characters}; number of lines: {num_lines}; number of words: {len(num_of_words)}")
-#input_calc()
-#
+
+def input_calc():
+    with open(r"test.txt", 'r') as file:
+        count = 0
+        for line in file:
+            if line != "\n":
+                count += 1
+    num_lines = count
+    file = open("test.txt", "r")
+    data = file.read()
+    num_of_characters = len(data)
+    num_of_words = data.split()
+    print(f"Number of characters: {num_of_characters}; number of lines: {num_lines}; number of words: {len(num_of_words)}")
+input_calc()
+
+
 #4
+
+
 def is_prime_numbers(num): 
     if num < 2:
         return False
