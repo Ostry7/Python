b01f0863fc431c9e709396a138b349e7d43bd991
Marek Ostrowski <ostrowskimarek7@outlook.com>
2025-02-17 14:40:52 +0100

    feat: add new API to select full country name

--- predicting_the_nationalityAPI.py
+++ predicting_the_nationalityAPI.py
@@ -2,23 +2,29 @@ import requests
 import json
 from operator import itemgetter
 
-#https://api.nationalize.io?name=johnson
+#https://api.nationalize.io?name=johnson    -> to guess the nationality
+#https://restcountries.com/v3.1/alpha/PL    -> to select full country name
 #100 requests/day
 
 
 name = input('Please enter your name to gess your nationality: ')
 
-response = requests.get(f'https://api.nationalize.io?name={name}')
+response_nationalize = requests.get(f'https://api.nationalize.io?name={name}')
 
-if response.status_code == 200:
-    data = response.json().get('country', [])
+if response_nationalize.status_code == 200:
+    data = response_nationalize.json().get('country', [])
     sorted_response = sorted(data, key=itemgetter('probability'), reverse=True)
     for index, item in enumerate(data):
-        if index == 0:
-            print (f"Your nationality is {item['country_id']}, in {int(100 * item['probability'])}%")
+        response_countries = requests.get(f"https://restcountries.com/v3.1/alpha/{item['country_id']}")
+        if response_countries.status_code == 200:
+            country_data = response_countries.json()
+            country_name = country_data[0]['name']['common']
+            if index == 0:
+                print (f"Your nationality is {country_name}, in {int(100 * item['probability'])}%")
+            else:
+                print (f"The rest nationality are {country_name}, in {int(100 * item['probability'])}%")
         else:
-            print (f"The rest nationality are {item['country_id']}, in {int(100 * item['probability'])}%")
-
+            print(f'Error: {response_countries.status_code}')
 else:
-    print (f'Error: {response.status_code}')
+    print (f'Error: {response_nationalize.status_code}')
 
