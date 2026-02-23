cbc0e7e3841c00c5874461f752fb25c2e299a93b
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-02-23 13:38:52 +0100

    add top 10 ip status errors

--- Task_1_Apache_Nginx_Log_Parser.py
+++ Task_1_Apache_Nginx_Log_Parser.py
@@ -1,8 +1,18 @@
 import re
+from collections import Counter
 
-txt = "192.168.68.1, QWERTY, 1234, 1235 13274"
+ip_reg = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+status_req_reg = r'"(?:GET|POST) .* (4\d{2}|5\d{2})'
+
 f = open("apache-logs/apache_logs.txt")
 
-ipregex = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", txt)
-print(ipregex)
+with open("apache-logs/apache_logs.txt") as f:
+    errors = []
+    for line in f:
+        ip_match = re.match(ip_reg,line)
+        status_match = re.search(status_req_reg,line)
+        if ip_match and status_match:
+            errors.append(ip_match.group())
+print (Counter(errors).most_common(10))
 
