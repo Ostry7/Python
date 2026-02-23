1b6a0d6b534ac1543110b8a594eb301675a13a13
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-02-23 13:55:18 +0100

    add exporting csv to Task1

--- Task_1_Apache_Nginx_Log_Parser.py
+++ Task_1_Apache_Nginx_Log_Parser.py
@@ -1,4 +1,4 @@
-import re
+import re, csv
 from collections import Counter
 
 ip_reg = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
@@ -7,6 +7,7 @@ status_req_reg = r'"(?:GET|POST) .* (4\d{2}|5\d{2})'
 
 f = open("apache-logs/apache_logs.txt")
 
+#parse logs
 with open("apache-logs/apache_logs.txt") as f:
     errors = []
     for line in f:
@@ -14,5 +15,17 @@ with open("apache-logs/apache_logs.txt") as f:
         status_match = re.search(status_req_reg,line)
         if ip_match and status_match:
             errors.append((ip_match.group(), status_match.group()))
-print (Counter(errors).most_common(10)) #TOP 10
+
+top_errors = (Counter(errors).most_common(10))  #TOP 10
+print ("TOP 10 errors:")
+for (ip,status), count in top_errors:
+    print(f"{ip}, ({status}): {count}")
+
+
+#export csv
+with open ("errors.csv", 'w', newline='') as errorfile:
+        writer = csv.writer(errorfile)
+        writer.writerow(['IP', 'Status_code', 'Occurrences'])
+        writer.writerows(top_errors)
+
 
