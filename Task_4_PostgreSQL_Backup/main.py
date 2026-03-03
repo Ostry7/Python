499e888d9e399bbb1d3283d7a57c13e62cd4e712
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-03 13:16:33 +0100

    add call upload_file() function

--- Task_4_PostgreSQL_Backup/main.py
+++ Task_4_PostgreSQL_Backup/main.py
@@ -1,4 +1,5 @@
-from pg_backup import ssh_connect_and_backup, gzip_backup 
+from pg_backup import ssh_connect_and_backup, gzip_backup
+from aws_s3_push import upload_file
 
 
 if __name__ == "__main__":
@@ -6,8 +7,13 @@ if __name__ == "__main__":
 
     if is_backup_ok:
         print ("[SUCCESS] Backup file successfully created! -> Compressing file: ")
-        gzip_backup()
+        is_gzip_ok = gzip_backup()
         print ("[SUCCESS] Compress backup file!")
+        if is_gzip_ok:
+            upload_file('backup.sql.gz', 'aws-python-bucket12341234')
+            print ("[SUCCESS] Successfully upload backup to the S3!")
+        else:
+            print ("[FAIL] Failed to upload backup to S3!")
     else:
         print("[FAIL] Backup failed! Skipping compression!")
         exit(1)
