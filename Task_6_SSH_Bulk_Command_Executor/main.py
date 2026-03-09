ff3299c878fc9d48f28cfd18816272b2c1b46d8f
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-09 14:59:27 +0100

    add exec_commands() and ssh_connect() functions

--- Task_6_SSH_Bulk_Command_Executor/main.py
+++ Task_6_SSH_Bulk_Command_Executor/main.py
@@ -1,16 +1,45 @@
 import paramiko
+import yaml
 
-# host config
-HOST_NAME = "192.168.68.240" #debian
-USER_NAME = "USER"
-PASSWORD = "PASSWORD"
-#PRIVATE_KEY_FILE = '/some/file/path/private_key.txt'
-PORT = 22
+with open("bulk.yaml", 'r') as f:
+    config = yaml.safe_load(f)
+hosts = config["hosts"]
+commands = config["commands"]
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(hostname=HOST_NAME, port=PORT, username=USER_NAME, password=PASSWORD)
+def ssh_connect(IP_ADDRESS,HOST_NAME,PORT,USER_NAME,PASSWORD):
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname=IP_ADDRESS, port=PORT, username=USER_NAME,  password=PASSWORD)
+        print(f"SUCCESSFULLY CONNECTED TO THE {HOST_NAME} !")
+        exec_commands(client, HOST_NAME)
+        client.close()
 
-command = "ls -l"
-stdin, stdout, stderr = client.exec_command(command)
-print(stdout.read())
+    except Exception as e:
+        print(f"Cannot connect to the {HOST_NAME}: {e}")
+
+def exec_commands(client, HOST_NAME):
+    try:
+        for command in commands:
+            stdin, stdout, stderr = client.exec_command(command)
+            output = stdout.read().decode()
+            error = stderr.read().decode()
+            print(f"[{HOST_NAME}] $ {command}")
+            print(output if output else error)
+    except Exception as e:
+        print(f"Cannot exec commands on {HOST_NAME}: {e}")
+    except:
+        print(f"Cannot exec commands!")
+
+for host in hosts:
+    ssh_connect(
+        host["ip"],
+        host["name"],
+        host["port"],
+        host["user"],
+        host["password"]
+    )
+
+
+
+    
