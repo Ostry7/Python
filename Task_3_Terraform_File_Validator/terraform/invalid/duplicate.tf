504ce8643405e3746b26f1bd95420aee7eb7e6a8
Marek Ostrowski <ostrowskimarek7@outlook.com>
2026-03-17 14:13:56 +0100

    refactor: remove duplicate Task folders from root

--- Task_3_Terraform_File_Validator/terraform/invalid/duplicate.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "duplicate-bucket"
-}
-
-resource "aws_s3_bucket" "example" {  # DUPLIKAT!
-  bucket = "duplicate-too"
-}
--- Task_3_Terraform_File_Validator/terraform/invalid/missing.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instancez" "invalid" {  # literówka!
-  ami           = "ami-123"
-  instance_type = "t2.micro"
-}
